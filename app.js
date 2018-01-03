b1bca18f9d6dfb318b348bd98f3e4953752e7931
Ron Louie Marte <ronlouiemarte@gmail.com>
2018-01-03 14:16:58 +0800

    'Test'

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.get('/', (request, response) => {
     message: "Hello github.com"
   });
 })
-app.listen(4000);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () =>{
+  console.log(`Listening to port ${PORT}`);
+});
